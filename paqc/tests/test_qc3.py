be50ad62c443bc782d21edb97ddc46b9dedc1fc9
Dieleman <FDieleman@uk.imshealth.com>
2017-11-09 14:22:23 +0000

    qc3

--- paqc/tests/test_qc3.py
+++ paqc/tests/test_qc3.py
@@ -7,13 +7,15 @@ from paqc.utils.config_utils import config_open
 @pytest.mark.parametrize("dict_config", [
     config_open("paqc/data/driver_dict_output.yml")[1]
 ])
-@pytest.mark.parametrize("df, expected", [
+@pytest.mark.parametrize("df, expected, ls_faults", [
     # Subset from data/qc_data.csv
-    # (pd.read_csv("paqc/tests/data/qc3_check1.csv"), True),
-    # Column name G has trailing s
-    # (pd.read_csv("paqc/tests/data/qc8_check2.csv"), False),
+    (pd.read_csv("paqc/tests/data/qc3_check1.csv"), True, None),
+    # column test_FLAG has negative float
+    (pd.read_csv("paqc/tests/data/qc3_check2.csv"), False, ["test_FLAG"]),
     # Added some special columns
-    # (pd.read_csv("paqc/tests/data/qc8_check3.csv"), True),
+    (pd.read_csv("paqc/tests/data/qc8_check3.csv"), False,
+     ["D_7245_AVG_CLAIM_FREQ", "D_V048_AVG_CLAIM_CNT"])
 ])
-def test_qc3(df, expected, dict_config):
-    assert qc3(df, dict_config).passed == expected
+def test_qc3(df, expected, ls_faults, dict_config):
+    rpi = qc3(df, dict_config)
+    assert (rpi.passed == expected) & (rpi.extra == ls_faults)
