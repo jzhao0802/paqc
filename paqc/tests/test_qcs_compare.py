ba653052b671f089ee030fe7a0a062f93889ca81
Dieleman <FDieleman@uk.imshealth.com>
2017-11-20 10:13:00 +0000

    test qc50

--- paqc/tests/test_qcs_compare.py
+++ paqc/tests/test_qcs_compare.py
@@ -8,6 +8,8 @@ DICT_CONFIG_9TO13 = config_open(
     "paqc/tests/data/qc9to13_driver_dict_output.yml")[1]
 DICT_CONFIG_48 = config_open(
     "paqc/tests/data/qc48_driver_dict_output.yml")[1]
+DICT_CONFIG_50 = config_open(
+    "paqc/tests/data/qc50_driver_dict_output.yml")[1]
 
 # 46
 @pytest.mark.parametrize("dict_config", [DICT_CONFIG_9TO13])
@@ -69,4 +71,26 @@ def test_qc48(df_old, df_new, expected, ls_faults, dict_config):
     rpi = qc48(df_old, df_new, dict_config)
     assert (rpi.passed == expected) & (rpi.extra == ls_faults)
 
-#TODO: Write test for qc50
+
+# 50
+@pytest.mark.parametrize("dict_config", [DICT_CONFIG_50])
+@pytest.mark.parametrize("df_old", [csv.read_csv(DICT_CONFIG_50,
+                                    "paqc/tests/data/suite2_df_old.csv")[1]])
+@pytest.mark.parametrize("df_new, expected, ls_faults", [
+    # identical to suite2_df_old.csv
+    (csv.read_csv(DICT_CONFIG_50, "paqc/tests/data/qc50_check1.csv")[1],
+     True, None),
+    # Deleted rows for both feature A and B, only in the number is high enough
+    # to make the difference in fraction of empty columns high enough
+    (csv.read_csv(DICT_CONFIG_50, "paqc/tests/data/qc50_check2.csv")[1],
+     False, ['B_flag', 'B_count', 'B_first_exp_dt',	'B_last_exp_dt',
+             'B_hcp_spec']),
+    # Added values in two rows for feature C in class 1, the change with
+    # original file is bigger than the threshold of 0.3.
+    (csv.read_csv(DICT_CONFIG_50, "paqc/tests/data/qc50_check3.csv")[1],
+     False, ['C_flag', 'C_count', 'C_first_exp_dt', 'C_last_exp_dt',
+             'C_hcp_spec'])
+])
+def test_qc50(df_old, df_new, expected, ls_faults, dict_config):
+    rpi = qc50(df_old, df_new, dict_config)
+    assert (rpi.passed == expected) & (rpi.extra == ls_faults)
