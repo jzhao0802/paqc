67053982846d0100d4e5d5271844203eb64f202e
Dieleman <FDieleman@uk.imshealth.com>
2017-11-29 18:22:53 +0000

    qc 20 and 21

--- paqc/qc_functions/qcs_all_data_others.py
+++ paqc/qc_functions/qcs_all_data_others.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 from paqc.report import report as rp
 from paqc.utils import utils
@@ -145,3 +146,68 @@ def qc19(df, dict_config):
     ls_idx_faulty = df[~(df[index_date_col] > date_limit)].index.tolist()
 
     return rp.ReportItem.init_conditional(ls_idx_faulty, dict_config['qc'])
+
+
+def qc20(df, dict_config):
+    """
+    FREQ variables are calculated by dividing the COUNT by the lookback length
+    in years, not days or months.
+
+    :param df:
+    :param dict_config:
+    :return:
+    """
+    try:
+        lookback_dys = dict_config['qc']['qc_params']['lookback_dys']
+        days_month_year = dict_config['qc']['qc_params']['days_months_years']
+    except KeyError as e:
+        return rp.ReportItem(passed=False,
+                             text='parameter missing under qc_params: %s' % e,
+                             **dict_config['qc'])
+    try:
+        cte = {'days': 1, 'month': 12, 'year': 365}[days_month_year]
+    except KeyError as e:
+        return rp.ReportItem(passed=False,
+                             text='days_month_year value %s not part of '
+                                  '[days, month, year]' % e,
+                             **dict_config['qc'])
+
+    dict_grouped_cols = utils.generate_dict_grouped_columns(df, dict_config,
+                                                            ['freq_cols',
+                                                             'count_cols'])
+    # Only run the test on predictors that have all those two columns
+    dict_grouped_cols = {predictor: dict_grouped for predictor, dict_grouped
+                         in dict_grouped_cols.items() if (len(dict_grouped) == 2)}
+
+    n_samples = 10000/len(dict_grouped_cols)
+    ls_feat_faulty = []
+    for feat, dict_feat in dict_grouped_cols:
+        col_count, col_freq = dict_feat['count'], dict_feat['freq']
+        df_sample = df[[lookback_dys, col_count, col_freq]].sample(n_samples)
+        is_not_same = (abs(df_sample[col_count]/df_sample[lookback_dys]*cte -
+                       df_sample[col_freq]) > df_sample[col_freq]*0.1).any()
+        if is_not_same:
+            ls_feat_faulty.append(feat)
+
+    return rp.ReportItem.init_conditional(ls_feat_faulty, dict_config['qc'])
+
+
+def qc21(df, dict_config):
+    """
+    The value of the date difference variables are always 0 or positive, and
+    never exceed the lookback length for that patient. 
+
+    :param df:
+    :param dict_config:
+    :return:
+    """
+    lookback_dys = dict_config['qc']['qc_params']['lookback_dys']
+    ls_columns = dict_config['qc']['qc_params']['list_columns']
+    ls_regex = ["%s$" % col for col in ls_columns]
+    prog = re.compile("(" + ")|(".join(ls_regex) + ")")
+    ls_colnames = [colname for colname in df if prog.search(colname)]
+    ss_bool =df[ls_colnames].apply(lambda x:  (x < 0) | (x > df[
+        lookback_dys])).any()
+    ls_cols_faulty = ss_bool[ss_bool].index.tolist()
+
+    return rp.ReportItem.init_conditional(ls_cols_faulty, dict_config['qc'])
